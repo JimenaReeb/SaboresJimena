92fd2b50e625c7c20c05e7562c98653b0075a17c
JimenaReeb <Ryotsen@gmail.com>
2021-12-06 20:55:31 -0300

    Primera versión

---
+++ src/saboresjimena/SaboresJimena.java
@@ -0,0 +1,9 @@
+package saboresjimena;
+public class SaboresJimena {
+    public static void main(String[] args) {
+        String sabores[]={"Dulce", "Salado","Amargo","Acido","Umami","Picante","Astringente","Starchy","Adiposo"};
+        for(int i=1;i<6;i++){
+            System.out.println("Sabor: "+sabores[i]);
+        }
+    }  
+}
