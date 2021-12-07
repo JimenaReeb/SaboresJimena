24dfb532926abe12c89a6f89bce317121bbeff72
Andrea Verne <andrea.verne@hotmail.com>
2021-12-06 21:25:31 -0300

    Cambios en el for para que me muestre todos lo sabores por más de que se agregue uno o varios

--- src/saboresjimena/SaboresJimena.java
+++ src/saboresjimena/SaboresJimena.java
@@ -1,9 +1,11 @@
 package saboresjimena;
+
 public class SaboresJimena {
+
     public static void main(String[] args) {
-        String sabores[]={"Dulce", "Salado","Amargo","Acido","Umami","Picante","Astringente","Starchy","Adiposo"};
-        for(int i=1;i<6;i++){
-            System.out.println("Sabor: "+sabores[i]);
+        String sabores[] = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo", "Nada"};
+        for (int i = 1; i < sabores.length; i++) {
+            System.out.println("Sabor: " + sabores[i]);
         }
-    }  
+    }
 }
